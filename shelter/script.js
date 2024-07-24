9f981529208b697efbbb2eb6aae5e54e8be94fcb
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2024-07-24 19:32:18 +0300

    feat: add active link to pets page

---
+++ shelter/script.js
@@ -0,0 +1,8 @@
+document.querySelector(".getToKnow").addEventListener("click", function () {
+  window.location.href = "pets.html";
+});
+document
+  .querySelector("header_nav a[href='#ourPets']")
+  .addEventListener("click", function () {
+    window.location.href = "pets.html";
+  });
