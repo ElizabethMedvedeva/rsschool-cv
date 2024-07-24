f54f934a0902b213fd0d9be677633ad402ff1998
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2024-07-24 20:14:29 +0300

    feat: add links connections

--- shelter/script.js
+++ shelter/script.js
@@ -6,3 +6,16 @@ document
   .addEventListener("click", function () {
     window.location.href = "pets.html";
   });
+document
+  .querySelector("header_nav a[href='#about']")
+  .addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+document
+  .querySelector(".header_logo.pets")
+  .addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+document.querySelector("header_nav a[href='#help']").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
